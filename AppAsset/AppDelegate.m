a22e73cc2888cf3ea76e6ef557b1a6a1b6343893
方益民 <fangyimin@nidexiaomimei.local>
2018-03-22 15:27:33 +0800

    我想说我会爱你多一点点

--- AppAsset/AppDelegate.m
+++ AppAsset/AppDelegate.m
@@ -22,6 +22,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    NSLog(@"真你诶");
+    
     [self for3DTouch];
     
     return YES;
